a38e6ed7b66f48cef931623c2e0e98a3cfc2b071
AntonioTrrs <antonio1959988@hotmail.com>
2024-03-25 12:37:50 -0600

    deploy reducer

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between items-center">
           <h1 className="text-center text-lg font-bold text-white uppercase">
-            Contador de Calorías 
+            ProtoCalories
           </h1>
 
           <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10" disabled={!canRestartApp()} onClick={() => dispatch({type: 'restart-app'})}>Reiniciar App</button>
